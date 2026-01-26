f3b6f2ee46cf579d6fc4921365327997ddf85d74
Gabewizard <TONIGABE05@GMAIL.COM>
2026-01-25 22:29:59 -0500

    Working Final

--- Lab1_gabet.cpp
+++ Lab1_gabet.cpp
@@ -5,3 +5,24 @@
  * @brief A simple program to greet the user by name.
  */
 
+#include <iostream>
+#include <string>
+
+int main() {
+    // Declare variables
+    std::string firstName;
+    std::string lastName;
+    
+    // Prompt and read first name
+    std::cout << "Enter your first name: ";
+    std::cin >> firstName;
+    
+    // Prompt and read last name
+    std::cout << "Enter your last name: ";
+    std::cin >> lastName;
+    
+    // Output greeting
+    std::cout << "Welcome to C++ Programming, " << firstName << " " << lastName << "!" << std::endl;
+    
+    return 0;
+}
