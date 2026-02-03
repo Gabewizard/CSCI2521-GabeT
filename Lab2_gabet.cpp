729eb7fe05c2d18a90d0e1cef8a36592638f8803
Gabewizard <TONIGABE05@GMAIL.COM>
2026-02-02 19:28:36 -0500

    LAB2

---
+++ Lab2_gabet.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file Lab2_username.cpp
+ * @author Gabriel Toninelli
+ * @date 2025-01-22
+ * @brief A program to calculate the perimeter of a rectangle from user input.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Declare floating-point variables
+    double length = 0.0;
+    double width = 0.0;
+    double perimeter = 0.0;
+
+    // Introductory message
+    cout << "This application will calculate the perimeter of a rectangle." << endl << endl;
+
+    // Get user input
+    cout << "Please enter the length of the rectangle: ";
+    cin >> length;
+
+    cout << "Please enter the width of the rectangle: ";
+    cin >> width;
+
+    // Calculate perimeter
+    perimeter = 2 * (length + width);
+
+    // Output result
+    cout << "The perimeter of the rectangle is: " << perimeter << endl;
+
+    return 0;
+}
